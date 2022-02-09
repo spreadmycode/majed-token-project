d6d8b8f33435cc2722686b59bbd403c1f7c37883
spreadmycode <91585048+spreadmycode@users.noreply.github.com>
2022-02-09 02:52:08 -0800

    Readme added

--- backend/constants.ts
+++ backend/constants.ts
@@ -1,7 +1,7 @@
 import * as anchor from '@project-serum/anchor';
 
+
 // Mutable
-export const CANDY_MACHINE_ID = "EZnaTuo58c1rP2fYKfSBFi3m6PwuW4bw8X7kRtyuEnov";
 export const RPC_HOST_URL = "https://sparkling-dry-thunder.solana-devnet.quiknode.pro/08975c8cb3c5209785a819fc9a3b2b537d3ba604/";
 export const CONNECTION = new anchor.web3.Connection(RPC_HOST_URL);
 export const PROGRAM_ID = new anchor.web3.PublicKey('37AyxDEWFDzYH7Hnri994AYEL8iMFJBG6A2uti9BetPZ');
